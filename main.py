e63d8bade2c39eaa69acc1a22ba543f8149fd160
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2024-09-29 21:18:15 +0300

    I search and calcul the product that brought the most income and total price for each product

--- main.py
+++ main.py
@@ -29,3 +29,13 @@ print(f"max country name: {max_country_name}, and max country count: {max_countr
 online_retail_dashboard['totalIncome'] = online_retail_dashboard['Quantity'] * online_retail_dashboard['UnitPrice']
 total_income = online_retail_dashboard.groupby('Country')['totalIncome'].sum().reset_index()
 print(total_income)
+
+"""
+1. Обчислити дохід для кожної транзакції, перемноживши кількість товарів на їхню ціну (Quantity × UnitPrice).  
+2. найти товар, який приніс найбільший дохід.
+"""
+
+print(online_retail_dashboard['totalIncome'])
+
+max_total_price = max(online_retail_dashboard['totalIncome'])
+print(f"The product that brought the most income: {max_total_price}")
