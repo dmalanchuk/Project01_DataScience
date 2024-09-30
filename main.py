fd0705d9d6ae2281d83c646f1e3152b837d230fd
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2024-09-30 11:35:42 +0300

    ending project

--- main.py
+++ main.py
@@ -39,3 +39,32 @@ print(online_retail_dashboard['totalIncome'])
 
 max_total_price = max(online_retail_dashboard['totalIncome'])
 print(f"The product that brought the most income: {max_total_price}")
+
+
+"""
+1.Порахувати загальну кількість проданих одиниць кожного товару. Це дозволить визначити, які товари є найбільш популярними.  
+2.визначити, який товар мав найбільші продажі за кількістю.  
+"""
+
+"""
+порахувати загальну кількість проданих одиниць кожного товару 
+
+оптимізований код знаходження загальної кількості проданих
+одиниць кожного товару. використання transform() в якому передаємо функцію "sum"
+та виводим результа, по ітогу нам навіть не протрібно використовувати цикл, та унікальні ключі.
+"""
+positive_data = online_retail_dashboard[online_retail_dashboard['Quantity'] > 0]
+
+result_table = positive_data.groupby('Description')['Quantity'].sum().reset_index()
+result_table.columns = ['Category', 'Total Quantity']
+result_table.sort_values('Total Quantity', ascending=True, inplace=True)
+print(result_table)
+
+"""
+товар який мав найбільше продажів за кількістю
+"""
+
+total_quantity = online_retail_dashboard.groupby('Description')['Quantity'].sum().reset_index()
+top_product = total_quantity.loc[total_quantity['Quantity'].idxmax()]
+
+print(top_product)
